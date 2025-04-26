9612e3fdb5fd510104dd937573988f16745fd665
Alisher Iliyev <alisherwashere@gmail.com>
2025-04-27 02:46:44 +0800

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
-
 export default function Home() {
   return (
-    <h1 className="bg-red-500">Hello</h1>
+    <>
+      <h1 className="h1-bold font-inter">Hello</h1>
+      <h1 className="h1-bold font-space-grotesk">Hello</h1>
+    </>
   );
 }
-a
